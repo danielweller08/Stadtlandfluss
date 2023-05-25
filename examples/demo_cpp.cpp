918980f53db0717fc65a26fd1fe99d66d0bcf59f
Daniel <s09dwell@uni-bonn.de>
2023-05-25 10:22:23 +0200

    Demo Implementation start

--- examples/demo_cpp.cpp
+++ examples/demo_cpp.cpp
@@ -1,4 +1,59 @@
 #include <stadt_land_fluss/controller.hpp>
+#include <stadt_land_fluss/board.hpp>
+#include <string>
+#include <iostream>
+    
+using namespace StadtLandFluss;
 
-int main () {
-}
+    int main () {
+
+        Controller controller;
+        Board board;
+
+        //Game Id for game one
+        int gameid_1;
+        
+
+        //Create players
+        int userToken_player1 = controller.get_userToken();
+        std::string name_player1 = "Simon";
+
+        int userToken_player2 = controller.get_userToken();
+        std::string name_player2 = "Damyan";
+
+        int userToken_player3 = controller.get_userToken();
+        std::string name_player3 = "Daniel";
+
+        // Create game
+        board = controller.create_game(name_player1, userToken_player1);
+
+        board.get_currentLetter();
+        
+        // Users join the game
+        controller.join_game(gameid_1, name_player2, userToken_player2);
+        controller.create_category(gameid_1, userToken_player1, "Stadt");
+        controller.create_category(gameid_1, userToken_player1, "Land");
+        controller.create_category(gameid_1, userToken_player1, "Fluss");
+
+
+        controller.submit_category(gameid_1, userToken_player1, "Stadt", "");
+        // controller.stop_game();     // Kein Spieler kann mehr Kategorien schreiben
+
+
+
+
+        // controller.start_game(gameid_1, controller.get_userToken());
+        // controller.stop_game();
+
+
+        // // Methoden
+        
+        // controller.delete_category();
+        // controller.get_game();
+        // controller.get_games();
+        
+        // controller.quit_game();
+        
+
+        return 0;
+    }
